75cee87a0c469f36992edeb6d95cdf18221f15db
JeKachan <jekachan7@gmail.com>
2016-02-11 20:06:39 +0200

    first commit

---
+++ SportLoto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SportLoto.Startup))]
+namespace SportLoto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
