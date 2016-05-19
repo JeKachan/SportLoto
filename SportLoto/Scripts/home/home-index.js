67a4ac73dc0104994d644fa06be6f37bab9ade15
JeKachan <jekachan7@gmail.com>
2016-05-19 21:20:15 +0300

    add new Layouts

---
+++ SportLoto/Scripts/home/home-index.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    var $tiles = $("#tiles center").fadeTo(0, 0);
+    $(window).scroll(function (d, h) {
+        $tiles.each(function (i) {
+            a = $(this).offset().top + $(this).height();
+            b = $(window).scrollTop() + $(window).height();
+            if (a < b) $(this).fadeTo(500, 1);
+        });
+    });
+    $("#kayitbuton").click(function () {
+        $("#bs-example-navbar-collapse-1").removeClass("in");
+    });
+})
+
