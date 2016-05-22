0c65675ed3666651c399a179c6582beda92c9e20
JeKachan <jekachan7@gmail.com>
2016-05-22 11:36:44 +0300

    added email registration confirm

---
+++ SportLoto/Models/EmailViewModels.cs
@@ -0,0 +1,15 @@
+﻿using Postal;
+
+namespace SportLoto.Models
+{
+    public class RegistrationEmail : Email
+    {
+        public string To { get; set; }
+        public string UserName { get; set; }
+        public string From { get; set; }
+        public string Subject { get; set; }
+        public string CallbackUrl { get; set; }
+
+        public RegistrationEmail(string viewName) : base(viewName) { }
+    }
+}
